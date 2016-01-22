d0c29ed88a4aa2a1563381042d7f99368e8c8c1c
xuxiaomeng <xuxiaomeng@baozou.com>
2016-01-22 18:11:07 +0800

    feature. add base logic.

--- src/defer.js
+++ src/defer.js
@@ -12,8 +12,52 @@
  */
 
 (function(global) {
+    var _temp = undefined;
+
+    var factory = function(func, key) {
+        var args = [];
+        var context = undefined;
+        var obj = function() {
+            if (typeof func !== 'function') { return; }
+            // if there are arguments input, we push them to the end of args.
+            var _args = [].slice.call(arguments);
+            if (_args.length == 0) {
+                _args = args;
+            }
+            var _context = context || this;
+            return func.apply(_context, _args);
+        }
+        obj.valueOf = function() {
+            args.push(_temp);
+            _temp = func;
+            return func.valueOf();
+        }
+        obj.toString = function() {
+            return func.toString();
+        }
+        if (typeof func === 'function') { 
+            obj.call = function() {
+                var _args = [].slice.call(arguments);
+                return func.apply(_args.shift(), _args);
+            };
+            obj.apply = function(v1, v2) {
+                return func.apply(v1, v2);
+            };
+            obj.currentArgs = function() {
+                return args;
+            };
+            obj.setContext = function(ctxt) {
+                context = ctxt;
+                return this;
+            }
+        }
+        obj.__defer__ = true;
+        return obj;
+    }
+
     function defer(value) {
-        
+        // let user decide the scope of context
+        return factory(value, typeof value);
     }
 
     global.defer = defer;
