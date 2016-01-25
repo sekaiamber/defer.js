fc8733117c020a8fa98531c805dd13d7517e2cdf
xuxiaomeng <xuxiaomeng@baozou.com>
2016-01-25 15:27:45 +0800

    feature. add commment.

--- src/defer.js
+++ src/defer.js
@@ -12,23 +12,32 @@
  */
 
 (function(global) {
+    // global key
     var _key = new Date().valueOf();
+    // cache
     var _temp = undefined;
-
+    // entrance
     var factory = function(func, key) {
+        // defer(a) and defer(defer(a)) have same return
         if (func && func.__defer__) {
             return func;
         }
+        // using closure to hide the arguments
         var args = [];
+        // return our defer function
         var obj = function() {
             if (typeof func !== 'function') { return; }
             var _args = [].slice.call(arguments);
+            // if the function get arguments, we use them to call the original function
             if (_args.length == 0) {
                 _args = args;
             }
             var _context = obj.context || this;
+            // we call the function immediately
             return func.apply(_context, _args);
         }
+        // important: when call 'a >> b' operator, js will call a.valueOf and b.valueOf
+        // we use valueOf() to pass and get data to cache
         obj.valueOf = function() {
             if (this.multiargs) {
                 _temp = args.concat([func]); 
@@ -40,11 +49,14 @@
                 var _context = this.context || this;
                 this.value = func.apply(_context, args);
             }
+            // hide our defer function
             return func.valueOf();
         }
+        // hide our defer function
         obj.toString = function() {
             return func.toString();
         }
+        // if the data is a function, we support 'call' and 'apply', and give a function to set context
         if (typeof func === 'function') {
             obj.call = function() {
                 var _args = [].slice.call(arguments);
@@ -72,18 +84,15 @@
             this.multiargs = true;
             return this;
         };
-        obj.getArgs = function() {
-            return args;
-        }
-        obj.__defer__ = func;
-        obj.__defer_key__ = key;
+        // give the sign and key
+        obj.__defer__ = key;
         return obj;
     }
 
     function defer(value) {
         var _args = [].slice.call(arguments);
         if (_args.length !== 1) {
-            // console.log(factory(_args.pop(), _key++).setAutorun(false));
+            // support defer(a, b, ...)
             var ret = factory(_args.pop(), _key++).setAutorun(false);
             ret.setMultiArgs.apply(ret, _args);
             return ret;
